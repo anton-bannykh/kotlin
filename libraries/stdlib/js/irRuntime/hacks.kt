b77100465a8563586a8d45a3170aa1e48e6f5225
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2018-08-23 18:19:01 +0300

    wip

--- libraries/stdlib/js/irRuntime/hacks.kt
+++ libraries/stdlib/js/irRuntime/hacks.kt
@@ -5,6 +5,8 @@
 
 package kotlin
 
+import kotlin.js.*
+
 // TODO: Ignore FunctionN interfaces
 
 public interface Function0<out R> : Function<R> {
@@ -23,5 +25,4 @@ public interface Function3<in P1, in P2, in P3, out R> : Function<R> {
     public operator fun invoke(p1: P1, p2: P2, p3: P3): R
 }
 
-public inline fun <reified T> arrayOfNulls(size: Int): Array<T?> = js("[]")  // FIXME: Implement
-
+public inline fun <reified T> arrayOfNulls(size: Int): Array<T?> = Array<T?>(size)
