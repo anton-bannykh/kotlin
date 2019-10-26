b363e140a33adb7eea878658760a2cc55eb0b008
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2019-10-26 15:57:25 +0300

    ~ Mappings

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/PersistentApi.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/PersistentApi.kt
@@ -8,6 +8,8 @@ package org.jetbrains.kotlin.ir.declarations
 import org.jetbrains.kotlin.ir.declarations.impl.IrBodyBase
 import org.jetbrains.kotlin.ir.declarations.impl.IrDeclarationBase
 import org.jetbrains.kotlin.ir.symbols.IrSymbol
+import kotlin.reflect.KMutableProperty0
+import kotlin.reflect.KProperty
 
 // TODO threadlocal
 var stageController: StageController = NoopController()
@@ -30,6 +32,8 @@ interface StageController {
     fun <T> withInitialIr(block: () -> T): T
 
     fun register(declaration: IrDeclarationBase<*>) {}
+
+    fun <K: IrDeclaration, V> getUserdata(declaration: IrDeclaration): MutableMap<MappingKey<K, V>, V>
 }
 
 class NoopController(override var currentStage: Int = 0) : StageController {
@@ -38,4 +42,42 @@ class NoopController(override var currentStage: Int = 0) : StageController {
         get() = true
 
     override fun <T> withInitialIr(block: () -> T): T = block()
+
+    private val userDataMap: MutableMap<IrDeclaration, MutableMap<MappingKey<IrDeclaration, Any>, Any>> = mutableMapOf()
+
+    override fun <K : IrDeclaration, V> getUserdata(declaration: IrDeclaration): MutableMap<MappingKey<K, V>, V> {
+        return userDataMap.getOrPut(declaration) { mutableMapOf<MappingKey<IrDeclaration, Any>, Any>() } as MutableMap<MappingKey<K, V>, V>
+    }
 }
+
+interface MappingKey<K : IrDeclaration, V>
+
+class MappingDelegate<K : IrDeclaration, V>(
+    val key: MappingKey<K, V>
+) {
+    private val K.map: MutableMap<MappingKey<K, V>, V>
+        get() = stageController.getUserdata(this)
+
+    operator fun getValue(thisRef: K, desc: KProperty<*>): V? {
+        stageController.lazyLower(thisRef)
+        return thisRef.map[key] as V?
+    }
+
+    operator fun setValue(thisRef: K, desc: KProperty<*>, value: V?) {
+        stageController.lazyLower(thisRef)
+        if (value == null) {
+            thisRef.map.remove(key)
+        } else {
+            thisRef.map[key] = value
+        }
+    }
+}
+
+fun <V : Any> KMutableProperty0<V?>.getOrPut(fn: () -> V) = this.get() ?: fn().also {
+    this.set(it)
+}
+
+fun <K : IrDeclaration, V> mapping(key: MappingKey<K, V>) = MappingDelegate(key)
+
+inline fun <K : IrDeclaration, V> mapping() = MappingDelegate(object : MappingKey<K, V> {})
+
