229e37340b143eaf529a423723ed69adc77460a3
Anton Bannykh <anton.bannykh@jetbrains.com>
2019-12-25 13:23:54 +0300

    PrimaryConstructorLowering

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/PrimaryConstructorLowering.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/PrimaryConstructorLowering.kt
@@ -5,46 +5,44 @@
 
 package org.jetbrains.kotlin.ir.backend.js.lower
 
+import org.jetbrains.kotlin.backend.common.BodyLoweringPass
 import org.jetbrains.kotlin.backend.common.ClassLoweringPass
 import org.jetbrains.kotlin.backend.common.CommonBackendContext
+import org.jetbrains.kotlin.backend.common.DeclarationTransformer
 import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.ir.builders.declarations.addConstructor
-import org.jetbrains.kotlin.ir.declarations.IrClass
-import org.jetbrains.kotlin.ir.declarations.IrConstructor
-import org.jetbrains.kotlin.ir.declarations.IrDeclaration
-import org.jetbrains.kotlin.ir.declarations.IrDeclarationOriginImpl
+import org.jetbrains.kotlin.ir.declarations.*
+import org.jetbrains.kotlin.ir.expressions.IrBody
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.IrInstanceInitializerCall
 import org.jetbrains.kotlin.ir.expressions.impl.IrBlockBodyImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrDelegatingConstructorCallImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrInstanceInitializerCallImpl
+import org.jetbrains.kotlin.ir.util.parentAsClass
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
 
 
+private var IrClass.syntheticPrimaryConstructor by mapping<IrClass, IrConstructor>()
+
 // Create primary constructor if it doesn't exist
-class PrimaryConstructorLowering(context: CommonBackendContext) : ClassLoweringPass {
-    private val unitType = context.irBuiltIns.unitType
+class PrimaryConstructorLowering(context: CommonBackendContext) : DeclarationTransformer {
 
-    override fun lower(irClass: IrClass) {
-        val constructors = irClass.declarations.filterIsInstance<IrConstructor>()
+    override fun transformFlat(declaration: IrDeclaration): List<IrDeclaration>? {
+        if (declaration is IrClass) {
+            val constructors = declaration.declarations.filterIsInstance<IrConstructor>()
 
-        if (constructors.any { it.isPrimary }) return
+            if (constructors.any { it.isPrimary }) return null
 
-        val primary = createPrimaryConstructor(irClass)
-
-        val initializeTransformer = object : IrElementTransformerVoid() {
-            override fun visitDeclaration(declaration: IrDeclaration) = declaration // optimize visiting
-
-            override fun visitInstanceInitializerCall(expression: IrInstanceInitializerCall) = expression.run {
-                IrDelegatingConstructorCallImpl(startOffset, endOffset, type, primary.symbol)
-            }
+            declaration.syntheticPrimaryConstructor = createPrimaryConstructor(declaration)
         }
 
-        constructors.forEach { it.transformChildrenVoid(initializeTransformer) }
+        return null
     }
 
     private object SYNTHETIC_PRIMARY_CONSTRUCTOR : IrDeclarationOriginImpl("SYNTHETIC_PRIMARY_CONSTRUCTOR")
+
+    private val unitType = context.irBuiltIns.unitType
 
     private fun createPrimaryConstructor(irClass: IrClass): IrConstructor {
         val declaration = irClass.addConstructor {
@@ -58,5 +56,23 @@ class PrimaryConstructorLowering(context: CommonBackendContext) : ClassLoweringP
         }
 
         return declaration
+    }
+}
+
+class DelegateToSyntheticPrimaryConstructor: BodyLoweringPass {
+    override fun lower(irBody: IrBody, container: IrDeclaration) {
+        if (container is IrConstructor && !container.isPrimary) {
+            container.parentAsClass.syntheticPrimaryConstructor?.let { primary ->
+                val initializeTransformer = object : IrElementTransformerVoid() {
+                    override fun visitDeclaration(declaration: IrDeclaration) = declaration // optimize visiting
+
+                    override fun visitInstanceInitializerCall(expression: IrInstanceInitializerCall) = expression.run {
+                        IrDelegatingConstructorCallImpl(startOffset, endOffset, type, primary.symbol)
+                    }
+                }
+
+                irBody.transformChildrenVoid(initializeTransformer)
+            }
+        }
     }
 }
