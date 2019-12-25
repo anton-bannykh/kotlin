3c6f8964069464641deba2842525f4e415936ce4
Anton Bannykh <anton.bannykh@jetbrains.com>
2019-12-25 13:10:30 +0300

    ObjectLowering

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/ObjectLowering.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/ObjectLowering.kt
@@ -5,94 +5,93 @@
 
 package org.jetbrains.kotlin.ir.backend.js.lower
 
-import org.jetbrains.kotlin.backend.common.CommonBackendContext
-import org.jetbrains.kotlin.backend.common.DeclarationContainerLoweringPass
-import org.jetbrains.kotlin.backend.common.FileLoweringPass
+import org.jetbrains.kotlin.backend.common.*
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
 import org.jetbrains.kotlin.backend.common.lower.irBlockBody
 import org.jetbrains.kotlin.backend.common.lower.irIfThen
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
-import org.jetbrains.kotlin.ir.backend.js.JsIrBackendContext
 import org.jetbrains.kotlin.ir.backend.js.ir.JsIrBuilder
 import org.jetbrains.kotlin.ir.builders.*
 import org.jetbrains.kotlin.ir.builders.declarations.buildField
-import org.jetbrains.kotlin.ir.declarations.IrClass
-import org.jetbrains.kotlin.ir.declarations.IrDeclarationContainer
-import org.jetbrains.kotlin.ir.declarations.IrFile
-import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
+import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.expressions.IrBlockBody
+import org.jetbrains.kotlin.ir.expressions.IrBody
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.IrGetObjectValue
-import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.ir.types.makeNullable
-import org.jetbrains.kotlin.ir.util.defaultType
-import org.jetbrains.kotlin.ir.util.isEffectivelyExternal
-import org.jetbrains.kotlin.ir.util.primaryConstructor
-import org.jetbrains.kotlin.ir.util.transformDeclarationsFlat
+import org.jetbrains.kotlin.ir.util.*
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
 import org.jetbrains.kotlin.name.Name
 
+private var IrClass.getInstanceFunctions by mapping<IrClass, IrSimpleFunction>()
+private var IrClass.instanceField by mapping<IrClass, IrField>()
+
 class ObjectDeclarationLowering(
-    val context: CommonBackendContext,
-    val objectToGetInstanceFunction: MutableMap<IrClassSymbol, IrSimpleFunction>
-) : DeclarationContainerLoweringPass {
-    override fun lower(irDeclarationContainer: IrDeclarationContainer) {
-        irDeclarationContainer.transformDeclarationsFlat { declaration ->
-            if (declaration !is IrClass || declaration.kind != ClassKind.OBJECT || declaration.isEffectivelyExternal())
-                return@transformDeclarationsFlat null
+    val context: CommonBackendContext
+) : DeclarationTransformer {
 
-            val getInstanceFun = getOrCreateGetInstanceFunction(objectToGetInstanceFunction, declaration)
+    override fun transformFlat(declaration: IrDeclaration): List<IrDeclaration>? {
+        if (declaration !is IrClass || declaration.kind != ClassKind.OBJECT || declaration.isEffectivelyExternal())
+            return null
 
-            val instanceField = buildField {
-                name = Name.identifier(declaration.name.asString() + "_instance")
-                type = declaration.defaultType.makeNullable()
-                isStatic = true
-            }.apply {
-                parent = declaration.parent
-                initializer = null  // Initialized with 'undefined'
-            }
-            val primaryConstructor = declaration.primaryConstructor!!
-            val body = primaryConstructor.body as IrBlockBody
+        val getInstanceFun = getOrCreateGetInstanceFunction(declaration)
 
-            // Initialize instance field in the beginning of the constructor because it can be used inside the constructor later
-            val initInstanceField = context.createIrBuilder(primaryConstructor.symbol).buildStatement(UNDEFINED_OFFSET, UNDEFINED_OFFSET) {
-                irSetField(null, instanceField, irGet(declaration.thisReceiver!!))
-            }
-            body.statements.add(0, initInstanceField)
-
-            getInstanceFun.body = context.createIrBuilder(getInstanceFun.symbol).irBlockBody(getInstanceFun) {
-                +irIfThen(
-                    irEqualsNull(irGetField(null, instanceField)),
-                    // Instance field initialized inside constructor
-                    irCallConstructor(primaryConstructor.symbol, emptyList())
-                )
-                +irReturn(irGetField(null, instanceField))
-            }
-
-            listOf(declaration, instanceField, getInstanceFun)
+        val instanceField = buildField {
+            name = Name.identifier(declaration.name.asString() + "_instance")
+            type = declaration.defaultType.makeNullable()
+            isStatic = true
+        }.apply {
+            parent = declaration.parent
+            initializer = null  // Initialized with 'undefined'
         }
+
+        declaration.instanceField = instanceField
+
+        val primaryConstructor = declaration.primaryConstructor!! // TODO find a way to trigger this correctly
+
+        getInstanceFun.body = context.createIrBuilder(getInstanceFun.symbol).irBlockBody(getInstanceFun) {
+            +irIfThen(
+                irEqualsNull(irGetField(null, instanceField)),
+                // Instance field initialized inside constructor
+                irCallConstructor(primaryConstructor.symbol, emptyList())
+            )
+            +irReturn(irGetField(null, instanceField))
+        }
+
+        return listOf(declaration, instanceField, getInstanceFun)
     }
 }
 
 class ObjectUsageLowering(
-    val context: CommonBackendContext,
-    val objectToGetInstanceFunction: MutableMap<IrClassSymbol, IrSimpleFunction>
-) : FileLoweringPass {
-    override fun lower(irFile: IrFile) {
-        irFile.transformChildrenVoid(object : IrElementTransformerVoid() {
+    val context: CommonBackendContext
+) : BodyLoweringPass {
+
+    override fun lower(irBody: IrBody, container: IrDeclaration) {
+        if (container is IrConstructor && container.isPrimary) {
+            val irClass = container.parentAsClass
+            irClass.instanceField?.let { instanceField ->
+                // Initialize instance field in the beginning of the constructor because it can be used inside the constructor later
+                val initInstanceField = context.createIrBuilder(container.symbol).buildStatement(UNDEFINED_OFFSET, UNDEFINED_OFFSET) {
+                    irSetField(null, instanceField, irGet(irClass.thisReceiver!!))
+                }
+                (irBody as IrBlockBody).statements.add(0, initInstanceField)
+            }
+        }
+
+        irBody.transformChildrenVoid(object : IrElementTransformerVoid() {
             override fun visitGetObjectValue(expression: IrGetObjectValue): IrExpression {
                 val obj: IrClass = expression.symbol.owner
                 if (obj.isEffectivelyExternal()) return expression
-                return JsIrBuilder.buildCall(getOrCreateGetInstanceFunction(objectToGetInstanceFunction, obj).symbol)
+                return JsIrBuilder.buildCall(getOrCreateGetInstanceFunction(obj).symbol)
             }
         })
     }
 }
 
-fun getOrCreateGetInstanceFunction(objectToGetInstanceFunction: MutableMap<IrClassSymbol, IrSimpleFunction>, obj: IrClass) =
-    objectToGetInstanceFunction.getOrPut(obj.symbol) {
+fun getOrCreateGetInstanceFunction(obj: IrClass) =
+    obj::getInstanceFunctions.getOrPut {
         JsIrBuilder.buildFunction(
             obj.name.asString() + "_getInstance",
             returnType = obj.defaultType,
