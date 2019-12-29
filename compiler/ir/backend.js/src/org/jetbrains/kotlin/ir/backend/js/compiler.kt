900437b984d03dc4692e94d4b5ff730471ab7a6d
Anton Bannykh <anton.bannykh@jetbrains.com>
2019-12-29 22:42:55 +0300

    ~ simulate persistence

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/compiler.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/compiler.kt
@@ -7,34 +7,23 @@ package org.jetbrains.kotlin.ir.backend.js
 
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.backend.common.phaser.PhaseConfig
+import org.jetbrains.kotlin.backend.common.phaser.PhaserState
 import org.jetbrains.kotlin.backend.common.phaser.invokeToplevel
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.ir.backend.js.export.isExported
 import org.jetbrains.kotlin.ir.backend.js.lower.moveBodilessDeclarationsToSeparatePlace
 import org.jetbrains.kotlin.ir.backend.js.transformers.irToJs.IrModuleToJsTransformer
 import org.jetbrains.kotlin.ir.backend.js.utils.JsMainFunctionDetector
 import org.jetbrains.kotlin.ir.backend.js.utils.NameTables
-import org.jetbrains.kotlin.ir.backend.js.utils.isJsExport
-import org.jetbrains.kotlin.ir.declarations.*
+import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.ir.declarations.NoopController
+import org.jetbrains.kotlin.ir.declarations.stageController
 import org.jetbrains.kotlin.ir.util.ExternalDependenciesGenerator
-import org.jetbrains.kotlin.ir.util.fqNameWhenAvailable
 import org.jetbrains.kotlin.ir.util.generateTypicalIrProviderList
-import org.jetbrains.kotlin.ir.util.isEffectivelyExternal
 import org.jetbrains.kotlin.ir.util.patchDeclarationParents
 import org.jetbrains.kotlin.library.KotlinLibrary
 import org.jetbrains.kotlin.library.resolver.KotlinLibraryResolveResult
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtFile
-import org.jetbrains.kotlin.utils.DFS
-
-fun sortDependencies(dependencies: Collection<IrModuleFragment>): Collection<IrModuleFragment> {
-    val mapping = dependencies.map { it.descriptor to it }.toMap()
-
-    return DFS.topologicalOrder(dependencies) { m ->
-        val descriptor = m.descriptor
-        descriptor.allDependencyModules.filter { it != descriptor }.map { mapping[it] }
-    }.reversed()
-}
 
 class CompilerResult(
     val jsCode: String?,
@@ -54,7 +43,8 @@ fun compile(
     generateFullJs: Boolean = true,
     generateDceJs: Boolean = false
 ): CompilerResult {
-    stageController = NoopController()
+    val controller = NoopController()
+    stageController = controller
 
     val (moduleFragment, dependencyModules, irBuiltIns, symbolTable, deserializer) =
         loadIr(project, files, configuration, allDependencies, friendDependencies)
@@ -83,9 +73,14 @@ fun compile(
 
     deserializer.finalizeExpectActualLinker()
 
-    moveBodilessDeclarationsToSeparatePlace(context, moduleFragment)
+    val phaserState = PhaserState<IrModuleFragment>()
+    phaseList.forEachIndexed { index, phase ->
+        controller.currentStage = index + 1
+        phase.invoke(phaseConfig, phaserState, context, moduleFragment)
+    }
 
-    jsPhases.invokeToplevel(phaseConfig, context, moduleFragment)
+//    jsPhases.invokeToplevel(phaseConfig, context, moduleFragment)
+
 
     val transformer = IrModuleToJsTransformer(context, mainFunction, mainArguments)
     return transformer.generateModule(moduleFragment, generateFullJs, generateDceJs)
