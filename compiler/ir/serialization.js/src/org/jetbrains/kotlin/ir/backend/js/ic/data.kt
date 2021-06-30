f45d5f44a1aca7430a12a6af381e0ecb78f3da65
Anton Bannykh <anton.bannykh@jetbrains.com>
2021-06-30 12:17:00 +0300

    ~ data serialization wip

--- compiler/ir/serialization.js/src/org/jetbrains/kotlin/ir/backend/js/ic/data.kt
+++ compiler/ir/serialization.js/src/org/jetbrains/kotlin/ir/backend/js/ic/data.kt
@@ -8,6 +8,14 @@ package org.jetbrains.kotlin.ir.backend.js.ic
 import org.jetbrains.kotlin.ir.backend.js.SerializedMappings
 import org.jetbrains.kotlin.ir.serialization.SerializedCarriers
 import org.jetbrains.kotlin.library.SerializedIrFile
+import org.jetbrains.kotlin.library.impl.IrArrayMemoryReader
+import org.jetbrains.kotlin.library.impl.IrArrayWriter
+import org.jetbrains.kotlin.library.impl.IrMemoryArrayWriter
+import java.io.File
+
+class SerializedIcData(
+    val files: Collection<SerializedIcDataForFile>,
+)
 
 class SerializedIcDataForFile(
     val file: SerializedIrFile,
@@ -16,12 +24,59 @@ class SerializedIcDataForFile(
     val order: SerializedOrder,
 )
 
-class SerializedIcData(
-    val files: Collection<SerializedIcDataForFile>,
-)
-
 class SerializedOrder(
     val topLevelSignatures: ByteArray,
     val containerSignatures: ByteArray,
     val declarationSignatures: ByteArray,
 )
+
+fun SerializedIcData.writeTo(dir: File) {
+    if (!dir.mkdirs()) error("Failed to create output dir: ${dir.absolutePath}")
+
+
+
+    files.sortedBy { it.file.path }.let { files ->
+        // .file
+        IrArrayWriter(files.map { it.file.fileData }).writeIntoFile(File(dir, "file.fileData").absolutePath)
+        IrArrayWriter(files.map { it.file.declarations }).writeIntoFile(File(dir, "file.declarations").absolutePath)
+        IrArrayWriter(files.map { it.file.types }).writeIntoFile(File(dir, "file.types").absolutePath)
+        IrArrayWriter(files.map { it.file.signatures }).writeIntoFile(File(dir, "file.signatures").absolutePath)
+        IrArrayWriter(files.map { it.file.strings }).writeIntoFile(File(dir, "file.strings").absolutePath)
+        IrArrayWriter(files.map { it.file.bodies }).writeIntoFile(File(dir, "file.bodies").absolutePath)
+        // .carriers
+        IrArrayWriter(files.map { it.carriers.signatures }).writeIntoFile(File(dir, "carriers.signatures").absolutePath)
+        IrArrayWriter(files.map { it.carriers.declarationCarriers }).writeIntoFile(File(dir, "carriers.declarationCarriers").absolutePath)
+        IrArrayWriter(files.map { it.carriers.bodyCarriers }).writeIntoFile(File(dir, "carriers.bodyCarriers").absolutePath)
+        IrArrayWriter(files.map { it.carriers.removedOn }).writeIntoFile(File(dir, "carriers.removedOn").absolutePath)
+        // .mappings
+        IrArrayWriter(files.map { it.mappings.keyBytes() }).writeIntoFile(File(dir, "mappings.keys").absolutePath)
+        IrArrayWriter(files.map { it.mappings.valueBytes() }).writeIntoFile(File(dir, "mappings.values").absolutePath)
+        // .order
+        IrArrayWriter(files.map { it.order.topLevelSignatures }).writeIntoFile(File(dir, "order.topLevelSignatures").absolutePath)
+        IrArrayWriter(files.map { it.order.containerSignatures }).writeIntoFile(File(dir, "order.containerSignatures").absolutePath)
+        IrArrayWriter(files.map { it.order.declarationSignatures }).writeIntoFile(File(dir, "order.declarationSignatures").absolutePath)
+    }
+}
+
+private fun SerializedMappings.keyBytes() = IrMemoryArrayWriter(mappings.map { it.keys }).writeIntoMemory()
+private fun SerializedMappings.valueBytes() = IrMemoryArrayWriter(mappings.map { it.values }).writeIntoMemory()
+
+
+fun File.readIcData(): SerializedIcData {
+    if (!this.isDirectory) error("Directory doesn't exist: ${this.absolutePath}")
+
+    // .file
+    val fileFileDataReader = IrArrayMemoryReader(File(this, "file.fileData").readBytes())
+    val fileDeclarationsReader = IrArrayMemoryReader(File(this, "file.declarations").readBytes())
+    val fileTypesReader = IrArrayMemoryReader(File(this, "file.types").readBytes())
+    val fileSignaturesReader = IrArrayMemoryReader(File(this, "file.signatures").readBytes())
+    val fileStringsReader = IrArrayMemoryReader(File(this, "file.strings").readBytes())
+    val fileBodiesReader = IrArrayMemoryReader(File(this, "file.bodies").readBytes())
+    // .carriers
+    val carriersSignaturesReader = IrArrayMemoryReader(File(this, "carriers.signatures").readBytes())
+    val carriersDeclarationCarriersReader = IrArrayMemoryReader(File(this, "carriers.declarationCarriers").readBytes())
+    val carriersBodyCarriersReader = IrArrayMemoryReader(File(this, "carriers.bodyCarriers").readBytes())
+    val carriersRemovedOnReader = IrArrayMemoryReader(File(this, "carriers.removedOn").readBytes())
+    // .mappings
+    val mappings
+}
