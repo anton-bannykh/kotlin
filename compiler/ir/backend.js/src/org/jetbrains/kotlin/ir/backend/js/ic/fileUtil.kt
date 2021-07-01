4b5543dfabc42df2d6b448cf9c7e288f1a6fd244
Anton Bannykh <anton.bannykh@jetbrains.com>
2021-07-01 18:28:21 +0300

    ~ store md5 hashes of klib directories

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/ic/fileUtil.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/ic/fileUtil.kt
@@ -10,10 +10,12 @@ import org.jetbrains.kotlin.analyzer.AbstractAnalyzerWithCompilerReport
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.ir.backend.js.MainModule
 import org.jetbrains.kotlin.library.KotlinLibrary
+import org.jetbrains.kotlin.library.impl.javaFile
 import org.jetbrains.kotlin.library.resolver.KotlinLibraryResolveResult
 import org.jetbrains.kotlin.name.FqName
 import java.io.File
 import java.io.PrintWriter
+import java.security.MessageDigest
 
 // TODO more parameters for lowerings
 fun buildCache(
@@ -27,15 +29,43 @@ fun buildCache(
     exportedDeclarations: Set<FqName> = emptySet(),
 ) {
 
+    // TODO: lower all the klibs
     val icData = if (allDependencies.getFullList().size == 1) {
         prepareSingleLibraryIcCache(project, analyzer, configuration, mainModule.lib, allDependencies, friendDependencies, exportedDeclarations)
     } else null
 
     icData?.writeTo(File(cachePath))
 
-    // TODO md5
-    // TODO perform lowerings
-    CacheInfo(cachePath, mainModule.lib.libraryFile.absolutePath, icData != null).save()
+    CacheInfo(cachePath, mainModule.lib.libraryFile.absolutePath, mainModule.lib.libraryFile.javaFile().md5(), icData != null).save()
+}
+
+private fun File.md5(): ULong {
+    val md5 = MessageDigest.getInstance("MD5")
+
+    fun File.process(prefix: String = "") {
+        if (isDirectory) {
+            this.listFiles()!!.sortedBy { it.name }.forEach {
+                md5.digest((prefix + it.name).toByteArray())
+                it.process(prefix + it.name + "/")
+            }
+        } else {
+            md5.digest(readBytes())
+        }
+    }
+
+    this.process()
+
+    val d = md5.digest()
+
+    return ((d[0].toULong() and 0xFFUL)
+            or ((d[1].toULong() and 0xFFUL) shl 8)
+            or ((d[2].toULong() and 0xFFUL) shl 16)
+            or ((d[3].toULong() and 0xFFUL) shl 24)
+            or ((d[4].toULong() and 0xFFUL) shl 32)
+            or ((d[5].toULong() and 0xFFUL) shl 40)
+            or ((d[6].toULong() and 0xFFUL) shl 48)
+            or ((d[7].toULong() and 0xFFUL) shl 56)
+            )
 }
 
 fun checkCaches(
@@ -65,19 +95,20 @@ fun checkCaches(
 }
 
 // TODO md5 hash
-data class CacheInfo(val path: String, val libPath: String, val hasIcData: Boolean) {
+data class CacheInfo(val path: String, val libPath: String, val md5: ULong, val hasIcData: Boolean) {
     fun save() {
         PrintWriter(File(File(path), "info")).use {
             it.println(libPath)
+            it.println(md5.toString(16))
             it.println(hasIcData)
         }
     }
 
     companion object {
         fun load(path: String): CacheInfo {
-            val (libPath, hasIcData) = File(File(path), "info").readLines()
+            val (libPath, md5, hasIcData) = File(File(path), "info").readLines()
 
-            return CacheInfo(path, libPath, hasIcData == "true")
+            return CacheInfo(path, libPath, md5.toULong(16), hasIcData == "true")
         }
     }
 }
